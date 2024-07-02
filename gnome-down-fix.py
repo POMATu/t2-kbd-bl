5742b585605089cb8c7ffa00ecfb74ec8ef8e418
Your Name <you@example.com>
2024-07-02 14:21:06 -0400

    changed to smarter path in python

--- gnome-down-fix.py
+++ gnome-down-fix.py
@@ -3,10 +3,11 @@
 from pydbus import SessionBus
 from gi.repository import GLib
 import subprocess
+import os
 
 def handle_signal(*args):
     print("D-Bus signal received:", args)
-    subprocess.run("/home/user/t2-kbd-bl/kbdbl.sh down", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    subprocess.run(script_dir + "/kbdbl.sh down", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
 bus = SessionBus()
 loop = GLib.MainLoop()
@@ -15,6 +16,7 @@ loop = GLib.MainLoop()
 bus.subscribe(iface="org.gnome.SettingsDaemon.Power.Keyboard", signal="BrightnessChanged", signal_fired=handle_signal)
 
 try:
+    script_dir = os.path.dirname(os.path.abspath(__file__))
     loop.run()
 except KeyboardInterrupt:
     loop.quit()
